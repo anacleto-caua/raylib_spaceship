d66eca4c62ea945a1112b12e94744a83eb5f9ee6
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-12-07 07:32:04 -0300

    Fix: Enhanced flight stick rotations

--- src/player.c
+++ src/player.c
@@ -10,10 +10,12 @@
 Camera3D PlayerCamera; // TODO: Find a more formal way to attach the PlayerCamera to a player
 Model PlayerModel;
 const Vector3 PLAYER_MODEL_LOCAL_FORWARD = (Vector3){1, 0, 0};
+const Vector3 PLAYER_CAMERA_OFFSET = (Vector3){ 0, 10, -25 };
 
 Vector3 PlayerPos = VEC_ZERO;
 Vector3 CameraPos = VEC_ZERO;
 Matrix FixMatrix;
+Quaternion PlayerRot = { 0 };
 
 float Throttle = 0;     // Throttle percent, how much both engines are working: 0 min to 1 max
 float MaxThrottle = 1;
@@ -86,9 +88,7 @@ void PlayerSpawn()
 void PlayerInit()
 {
     PlayerPos = VEC_ZERO;
-    CameraPos = PlayerPos;
-    CameraPos.z -= 25;
-    CameraPos.y += 10;
+    CameraPos = Vector3Add(PlayerPos, PLAYER_CAMERA_OFFSET);
 
     PlayerCamera.position = CameraPos;
     PlayerCamera.target = PlayerPos;
@@ -105,6 +105,7 @@ void PlayerInit()
     FixMatrix = MatrixMultiply(FixMatrix, MatrixTranslate(PlayerPos.x, PlayerPos.y, PlayerPos.z));
     
     PlayerModel.transform = FixMatrix;
+    PlayerRot = QuaternionIdentity();
 
     AddDebugMsg("Speed: ", TYPE_FLOAT, &Speed);
     AddDebugMsg("Throttle: ", TYPE_FLOAT, &Throttle);
@@ -119,28 +120,22 @@ void PlayerUpdate()
     HandleMovementInput();
 
     // rotation
-    Vector3 rot =
-        Vector3Scale(
-            (Vector3)
-            {
-                FlightStickInput.y * pitchSpeed,
-                FlightStickInput.x * yawSpeed,
-                0 
-            },
-            GetFrameTime() * maneuverability
-        );
+    Quaternion qPitch = QuaternionFromAxisAngle(VEC_RIGHT, FlightStickInput.y * pitchSpeed * GetFrameTime());
+    Quaternion qYaw   = QuaternionFromAxisAngle(VEC_UP, FlightStickInput.x * yawSpeed * -GetFrameTime());
+    Quaternion qRoll  = QuaternionFromAxisAngle(VEC_FORWARD, 0.0f);
     
-    Matrix matrixSpin = MatrixRotateXYZ((Vector3){
-        rot.x * DEG2RAD,
-        rot.y * DEG2RAD,
-        rot.z * DEG2RAD 
-    });
+    // Combine them (Yaw -> Pitch -> Roll is standard order)
+    Quaternion qFrame = QuaternionMultiply(qYaw, QuaternionMultiply(qPitch, qRoll));
+    PlayerRot = QuaternionMultiply(PlayerRot, qFrame);
+    PlayerRot = QuaternionNormalize(PlayerRot);
+    Matrix matRotation = QuaternionToMatrix(PlayerRot);
+    PlayerModel.transform = MatrixMultiply(FixMatrix, matRotation);
 
-    Matrix TranformMatrix = MatrixMultiply(FixMatrix, matrixSpin);
-    PlayerModel.transform = TranformMatrix;
+    Vector3 currentForward = Vector3RotateByQuaternion(VEC_FORWARD, PlayerRot);
+    Vector3 rotatedOffset = Vector3RotateByQuaternion(PLAYER_CAMERA_OFFSET, PlayerRot);
 
     // movement
-    Vector3 PlayerMovement = Vector3Scale(VEC_FORWARD, Speed * GetFrameTime());
+    Vector3 PlayerMovement = Vector3Scale(currentForward, Speed * GetFrameTime());
 
     PlayerPos = Vector3Add(PlayerPos, PlayerMovement);
     CameraPos = Vector3Add(CameraPos, PlayerMovement);
