216fdd8d58cb8af0751384ddfec6b31f07595a3a
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-12-07 11:17:59 -0300

    Movement uses better physics

--- src/player.c
+++ src/player.c
@@ -1,76 +1,80 @@
 #include "player.h"
-#include <stdio.h>
 #include "debugger/debugger.h"
 #include "game.h"
+#include "utils.h"
 #include "entity.h"
 #include "raylib.h"
 #include "raymath.h"
-#include "utils.h"
+#include <stdio.h>
 
+// config
+const float ENGINE_POWER = 100;       // Acceleration force
+const float DRAG_FACTOR = -.5;         // Air resistance (Higher = stops faster)
+const float MAX_VELOCITY = 100;      // Terminal velocity clamp
+const float ROTATION_SMOOTHING = 5;  // Lower = heavier plane, Higher = snappy
+const float ROLL_LERP = 1;
+const float CAMERA_LERP = 10;
+
+// input configs
+// TODO: Consider -1 for reverse
+const float MAX_THROTTLE = 1;
+const float MIN_THROTTLE = 0;
+const float THROTTLE_ACCELERATION = .7;
+const float THROTTLE_DE_ACCELERATION = .5;
+
+const float PITCH_SENSI = .7;
+const float ROLL_SENSI = 1;
+const float YAW_SENSI = 1;
+
+// state
 Camera3D PlayerCamera; // TODO: Find a more formal way to attach the PlayerCamera to a player
 Model PlayerModel;
 const Vector3 PLAYER_MODEL_LOCAL_FORWARD = (Vector3){1, 0, 0};
-const Vector3 PLAYER_CAMERA_OFFSET = (Vector3){ 0, 10, -25 };
+const Vector3 PLAYER_CAMERA_OFFSET = (Vector3){ 0, 5, -25 };
 
 Vector3 PlayerPos = VEC_ZERO;
+Vector3 PlayerVelocity = VEC_ZERO;
 Vector3 CameraPos = VEC_ZERO;
 Matrix FixMatrix;
 Quaternion PlayerRot = { 0 };
 
+// input
 float Throttle = 0;     // Throttle percent, how much both engines are working: 0 min to 1 max
-float MaxThrottle = 1;
-float ThrottleAcceleration = .7;
-float ThrottleDeAcc = .5;
-
-const float MAX_SPEED = 700000000;
-float Speed = 0;
-float AirResistance = .7;
-float SpeedGainOnMaxThrottle = 50;
-
-float ThrottleAsymmetry = 0;
-float ThrottleAsymAcc = 0;
-
 Vector2 FlightStickInput = VEC2_ZERO;
 float RollInput = 0;
-float RollSensi = 1000;
-float RollDecay = .01;
 
-float pitchSpeed = 1;
-float rollSpeed = 1;
-float yawSpeed = 1;
 
-float maneuverability = 1000;
+float ft = 0; // GetFrameTime();
 
 // helpers
 
 void HandleMovementInput()
 {
-    if(IsKeyDown(KEY_W))
+    if (IsKeyDown(KEY_W)) 
     {
-        Throttle += ThrottleAcceleration * GetFrameTime();
+        Throttle += THROTTLE_ACCELERATION * ft;
     }
-    else if(IsKeyDown(KEY_S))
+    else if (IsKeyDown(KEY_S)) 
     {
-        Throttle -= ThrottleDeAcc * GetFrameTime();
+        Throttle -= THROTTLE_DE_ACCELERATION * ft;
     }
+    cap(&Throttle, MIN_THROTTLE, MAX_THROTTLE);
 
+    float targetRoll = 0;
     if(IsKeyDown(KEY_D))
     {
-        RollInput += RollSensi;
+        targetRoll = 1;
     }
     else if(IsKeyDown(KEY_A))
     {
-        RollInput -= RollSensi;
+        targetRoll = -1;
     }
-    RollInput *= RollDecay;
+    RollInput = Lerp(RollInput, targetRoll, ROLL_LERP * ft);
 
     // Gets the mouse input from -1 to 1
     float inputX = (((float)GetMouseX() / GetScreenWidth())  - .5) * 2;
     float inputY = (((float)GetMouseY() / GetScreenHeight()) - .5) * 2;
     FlightStickInput = (Vector2){ inputX * inputX * inputX, inputY * inputY * inputY };
-
-    cap(&Throttle, 0.0f, MaxThrottle);
-    cap(&RollInput, -1.0f, +1.0f);
 }
 
 // public
@@ -83,6 +87,8 @@ void PlayerSpawn()
 void PlayerInit()
 {
     PlayerPos = VEC_ZERO;
+    PlayerVelocity = VEC_ZERO;
+    PlayerRot = QuaternionIdentity();
     CameraPos = Vector3Add(PlayerPos, PLAYER_CAMERA_OFFSET);
 
     PlayerCamera.position = CameraPos;
@@ -102,46 +108,75 @@ void PlayerInit()
     PlayerModel.transform = FixMatrix;
     PlayerRot = QuaternionIdentity();
 
-    AddDebugMsg("Speed: ", TYPE_FLOAT, &Speed);
     AddDebugMsg("Throttle: ", TYPE_FLOAT, &Throttle);
     
     AddDebugMsg("RollInput: ", TYPE_FLOAT, &RollInput);
     AddDebugMsg("FlightStick: ", TYPE_VECTOR2, &FlightStickInput);
 
     AddDebugMsg("PlayerPos: ", TYPE_VECTOR3, &PlayerPos);
+    AddDebugMsg("PlayerVel: ", TYPE_VECTOR3, &PlayerVelocity);
 }
 
 void PlayerUpdate()
 {
-    float ft = GetFrameTime();
+    ft = GetFrameTime();
+
     HandleMovementInput();
 
     // rotation
-    Quaternion qPitch = QuaternionFromAxisAngle(VEC_RIGHT, FlightStickInput.y * pitchSpeed * ft);
-    Quaternion qYaw   = QuaternionFromAxisAngle(VEC_UP, FlightStickInput.x * yawSpeed * -ft);   //TODO: Find a better way to invert the yaw axis
-    Quaternion qRoll  = QuaternionFromAxisAngle(VEC_FORWARD, RollInput * rollSpeed * ft);
+    Quaternion qPitch = QuaternionFromAxisAngle((Vector3){1,0,0}, FlightStickInput.y * PITCH_SENSI * ft);
+    Quaternion qYaw   = QuaternionFromAxisAngle((Vector3){0,1,0}, FlightStickInput.x * YAW_SENSI * -ft);     //TODO: Consider a cleaner method to invert the yaw axis
+    Quaternion qRoll  = QuaternionFromAxisAngle((Vector3){0,0,1}, RollInput * ROLL_SENSI * ft);
     
     // Combine them (Yaw -> Pitch -> Roll is standard order)
     Quaternion qFrame = QuaternionMultiply(qYaw, QuaternionMultiply(qPitch, qRoll));
+
     PlayerRot = QuaternionMultiply(PlayerRot, qFrame);
     PlayerRot = QuaternionNormalize(PlayerRot);
+
     Matrix matRotation = QuaternionToMatrix(PlayerRot);
     PlayerModel.transform = MatrixMultiply(FixMatrix, matRotation);
 
     Vector3 currentForward = Vector3RotateByQuaternion(VEC_FORWARD, PlayerRot);
     Vector3 rotatedOffset = Vector3RotateByQuaternion(PLAYER_CAMERA_OFFSET, PlayerRot);
 
-    // movement
-    Speed += Throttle * SpeedGainOnMaxThrottle;
-    Speed *= AirResistance;
-    Vector3 PlayerMovement = Vector3Scale(currentForward, Speed * ft);
+    // physics
+    Vector3 forceThrust = Vector3Scale(currentForward, Throttle * ENGINE_POWER);
+    Vector3 forceDrag = Vector3Scale(PlayerVelocity, DRAG_FACTOR);
+    
+    // TODO: Consider gravity
+    Vector3 forceGravity = Vector3Scale(VEC_UP, -9.8);
 
-    PlayerPos = Vector3Add(PlayerPos, PlayerMovement);
-    CameraPos = Vector3Add(PlayerPos, rotatedOffset);;
-    PlayerCamera.position = CameraPos;
+    Vector3 totalForce = Vector3Add(forceThrust, forceDrag);
+    totalForce = Vector3Add(totalForce, forceGravity);
+    Vector3 acceleration = totalForce;
+
+    PlayerVelocity = Vector3Add(PlayerVelocity, Vector3Scale(acceleration, ft));
+    PlayerPos = Vector3Add(PlayerPos, Vector3Scale(PlayerVelocity, ft));
+
+    // movement
+    // fix rotation
+    Quaternion fixQ = QuaternionFromVector3ToVector3(PLAYER_MODEL_LOCAL_FORWARD, VEC_FORWARD);
+    Matrix matFix = QuaternionToMatrix(fixQ);
+    Matrix matRot = QuaternionToMatrix(PlayerRot);
+    Matrix matTrans = MatrixTranslate(PlayerPos.x, PlayerPos.y, PlayerPos.z);
+    // Combine: Fix -> Rotate -> Translate
+    PlayerModel.transform = MatrixMultiply(matFix, MatrixMultiply(matRot, matTrans));
+
+    // camera
+    Vector3 camDesiredOffset = Vector3RotateByQuaternion(PLAYER_CAMERA_OFFSET, PlayerRot);
+    Vector3 desiredCamPos = Vector3Add(PlayerPos, camDesiredOffset);
+    PlayerCamera.position = Vector3Lerp(PlayerCamera.position, desiredCamPos, CAMERA_LERP * ft);
     PlayerCamera.target = PlayerPos;
 
-    DrawModelWires(PlayerModel, PlayerPos, 1, PURPLE);
+    // draw
+    DrawModelWires(PlayerModel,VEC_ZERO, 1, PINK);
+
+    // debug
+    // Draw Velocity Vector (Visualizing Drift)
+    DrawLine3D(PlayerPos, Vector3Add(PlayerPos, PlayerVelocity), RED);
+    // Draw Facing Vector
+    DrawLine3D(PlayerPos, Vector3Add(PlayerPos, Vector3Scale(currentForward, 10)), GREEN);
 }
 
 void PlayerEnd()
